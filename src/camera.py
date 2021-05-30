5df9b61838f87d1b4290190028dbce1b2c3f7083
Alexandre Martuscelli Faria <martuscellifaria@gmail.com>
2021-05-30 10:41:48 -0300

    Update camera.py

--- src/camera.py
+++ src/camera.py
@@ -1,37 +1,32 @@
 import cv2 as cv
+import sys, os
+from starman_jr import send_photo
+from dotenv import load_dotenv
+import time
 
 class VideoCamera(object):
     def __init__(self):
-        # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
-        self.video = cv.VideoCapture(0)
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
-        # self.video = cv2.VideoCapture('video.mp4')
+        self.video = cv.VideoCapture(1)
 
     def __del__(self):
         self.video.release()
 
     def get_frame(self):
         success, image = self.video.read()
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
         ret, jpeg = cv.imencode('.jpg', image)
         return jpeg.tobytes()
 
     def identify_camera(self):
-        #cap = self.video.read() #cv.VideoCapture(0)
-        #cap.set(3,640)
-        #cap.set(4,480)
+        load_dotenv()
+        telegram = os.getenv('telegram')
         classNames = []
         classFile = 'coco.names'
         with open(classFile, 'rt') as f:
             classNames = f.read().rstrip('\n').split('\n')
 
-        configpath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-        weigthspath = 'frozen_inference_graph.pb'
+        #Custom config and weights path for the Mustang
+        configpath = 'custom-yolov4-detector.cfg'
+        weigthspath = 'custom-yolov4-detector_best.weights'
 
         net = cv.dnn_DetectionModel(weigthspath, configpath)
         net.setInputSize(320,320)
@@ -39,22 +34,42 @@ class VideoCamera(object):
         net.setInputMean((127.5, 127.5, 127.5))
         net.setInputSwapRB(True)
 
-       # while True:
+        #Standard configs and weights path
+        configpath2 = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+        weigthspath2 = 'frozen_inference_graph.pb'
+
+        net2 = cv.dnn_DetectionModel(weigthspath2, configpath2)
+        net2.setInputSize(320,320)
+        net2.setInputScale(1.0 / 127.5)
+        net2.setInputMean((127.5, 127.5, 127.5))
+        net2.setInputSwapRB(True)
+
         self.video.set(3,640)
         self.video.set(4,480)
         success, img = self.video.read()
-        #img = cv.flip(img,1)
-        #img.set(3,640)
-        #img.set(4,480)
+        count = 0
         classIds, confs, bbox = net.detect(img, confThreshold=0.5)
         if len(classIds) != 0:
             for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
                 cv.rectangle(img, box, color=(0, 0, 255),thickness=2)
                 cv.putText(img, classNames[classId-1].upper(), (box[0]+10, box[1]+30), cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
-
-        #cv.imshow('Output', img)
-        #cv.waitKey(1)
+                if classNames[classId-1] == 'mustang':
+                    count += 1
+                    print(f'Count {count}')
+                    if count >= 4:
+                        print('Found the Mustang')
+                        cv.imwrite('temp/mustang.png', img)
+                        send_photo('temp/mustang.png', 'Found the Mustang!', telegram)
+                        os.remove('temp/mustang.png')
+                        count = 0
+                        time.sleep(15)
         
+        classIds2, confs2, bbox2 = net2.detect(img, confThreshold=0.5)
+        if len(classIds2) != 0:
+            for classId2, confidence2, box2 in zip(classIds2.flatten(), confs2.flatten(), bbox2):
+                cv.rectangle(img, box2, color=(0, 0, 255),thickness=2)
+                cv.putText(img, classNames[classId2-1].upper(), (box2[0]+10, box2[1]+30), cv.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+
+
         ret, jpeg = cv.imencode('.jpg', img)
         return jpeg.tobytes()
-        #return
